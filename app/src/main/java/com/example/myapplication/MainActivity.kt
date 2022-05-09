303d1e45a71327515763d20489bfd26bd5db7245
di0n0s <sfuentecarrique@gmail.com>
2022-05-09 14:40:09 +0200

    Remove userIntent and make mutableStateFlow lazy

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,46 +1,48 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: UserViewModel by viewModels()
+    private val viewModel: UserViewModel by viewModels { UserViewModel.UserViewModelFactory("2") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         collectState()
-        getUser()
-    }
-
-    private fun getUser() {
-        lifecycleScope.launch {
-            viewModel.userIntent.send(UserIntent.GetUser("2"))
-        }
     }
 
     private fun collectState() {
         lifecycleScope.launch {
             viewModel.userState.collect {
                 when (it) {
-                    is GetUserState.Error -> Log.e("userState", "error")
                     GetUserState.Idle -> {}
-                    GetUserState.Loading -> Log.e("userState", "Loading")
+                    is GetUserState.Error -> showError(it.error)
+
                     is GetUserState.Success -> renderUI(it.user)
                 }
             }
         }
+    }
 
+
+    private fun showError(error: String?) {
+        error?.let {
+            Snackbar.make(
+                binding.root,
+                error, Snackbar.LENGTH_LONG
+            ).show()
+        }
     }
 
     private fun renderUI(user: User) {
--- app/src/main/java/com/example/myapplication/UserViewModel.kt
+++ app/src/main/java/com/example/myapplication/UserViewModel.kt
@@ -1,18 +1,20 @@
 package com.example.myapplication
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.consumeAsFlow
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class UserViewModel(private val iODispatcher: CoroutineDispatcher = Dispatchers.IO) : ViewModel() {
+class UserViewModel(
+    private val id: String,
+    private val iODispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel() {
 
     private val apiService: ApiService = Retrofit.Builder()
         .baseUrl("https://reqres.in/api/")
@@ -20,30 +22,18 @@ class UserViewModel(private val iODispatcher: CoroutineDispatcher = Dispatchers.
         .build()
         .create(ApiService::class.java)
 
-    val userIntent = Channel<UserIntent>(Channel.UNLIMITED)
 
-    private val _userState = MutableStateFlow<GetUserState>(GetUserState.Idle)
+    private val _userState: MutableStateFlow<GetUserState> by lazy {
+        MutableStateFlow<GetUserState>(GetUserState.Idle).also {
+            getUser(id)
+        }
+    }
     val userState: StateFlow<GetUserState>
         get() = _userState
 
-    init {
-        handleUserIntent()
-    }
-
-    private fun handleUserIntent() {
-        viewModelScope.launch {
-            userIntent.consumeAsFlow().collect {
-                when (it) {
-                    is UserIntent.GetUser -> getUser(it.id)
-                }
-            }
-        }
-    }
 
     private fun getUser(id: String) {
         viewModelScope.launch(iODispatcher) {
-            _userState.value = GetUserState.Loading
-
             _userState.value = try {
                 val data = apiService.getUser(id).data
                 GetUserState.Success(data)
@@ -54,15 +44,15 @@ class UserViewModel(private val iODispatcher: CoroutineDispatcher = Dispatchers.
 
     }
 
-}
+    @Suppress("UNCHECKED_CAST")
+    class UserViewModelFactory(private val userId: String) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T = UserViewModel(userId) as T
+    }
 
-sealed class UserIntent {
-    class GetUser(val id: String) : UserIntent()
 }
 
 sealed class GetUserState {
     object Idle : GetUserState()
-    object Loading : GetUserState()
     class Success(val user: User) : GetUserState()
     class Error(val error: String?) : GetUserState()
 }
